9044dbcd648062598f541a48e525490c324b1dfc
Naman Deep <namandeep444@gmail.com>
2024-12-06 15:46:03 +0530

    Initial Commit

---
+++ Weblink/WeblinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeblinkApp.swift
+//  Weblink
+//
+//  Created by Naman Deep on 06/12/24.
+//
+
+import SwiftUI
+
+@main
+struct WeblinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
